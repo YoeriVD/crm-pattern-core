7f0a3c4c31d45e921834639e257fb771c5d76bb6
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-12-07 12:29:29 +0100

    detail working

--- crm-pattern.core/CrmFaker.cs
+++ crm-pattern.core/CrmFaker.cs
@@ -4,7 +4,9 @@ namespace crm_pattern.core
 {
     public static class CrmFaker
     {
-        private static int Id = 0;
+        private static int ContactId = 0;
+        private static int YouthHostelId = 0;
+        
         public static Faker<AddressValue> Address { get; } =
             new Faker<AddressValue>()
                 .RuleFor(a => a.Street, f => f.Address.StreetName())
@@ -12,12 +14,12 @@ namespace crm_pattern.core
                 .RuleFor(a => a.Number, f => f.Address.BuildingNumber());
 
         public static Faker<ContactPerson> ContactPerson { get; } = new Faker<ContactPerson>()
-            .RuleFor(p => p.Id, f => ++Id)
+            .RuleFor(p => p.Id, f => ++ContactId)
             .RuleFor(p => p.Name, f => f.Person.FirstName)
             .RuleFor(p => p.Surname, f => f.Person.LastName);
 
         public static Faker<YouthHostel> YouthHostel { get; } = new Faker<YouthHostel>()
             .RuleFor(y => y.Name, f => f.Company.CompanyName())
-            .RuleFor(y => y.Id, f => ++Id);
+            .RuleFor(y => y.Id, f => ++YouthHostelId);
     }
 }
