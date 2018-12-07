1a2ace20c747cb3f4029f2638169b0ab15b433d4
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-12-07 12:28:45 +0100

    detail working

---
+++ crm-pattern.web/Pages/Detail.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus.Platform;
+using crm_pattern.core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyApp.Namespace
+{
+    public class DetailModel : PageModel
+    {
+        private readonly CrmContext _db;
+
+        public DetailModel(CrmContext db)
+        {
+            _db = db;
+            _db.Database.EnsureCreated();
+        }
+
+        [BindProperty(SupportsGet = true)] public string Type { get; set; } = nameof(YouthHostel);
+        [BindProperty(SupportsGet = true)] public int Id { get; set; } = 1;
+
+        public Entity Entity { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var possibleTypes = _db.GetType().GetAssembly().GetTypes()
+                .Where(t => !string.IsNullOrWhiteSpace(t.Name))
+                .Where(t => t.IsSubclassOf(typeof(Entity)))
+                .ToDictionary(t => t.Name.ToLowerInvariant());
+            var lowerType = Type.ToLowerInvariant();
+
+            if (!possibleTypes.ContainsKey(lowerType)) throw new ArgumentException();
+            var type = possibleTypes[lowerType];
+
+
+            var set = _db.Set(type).Cast<Entity>();
+            var entity = await set.FirstOrDefaultAsync(t => t.Id == Id);
+            Entity = entity;
+        }
+    }
+}
